777d7ed49ccffd5c6ccd7d393e040cfaf9e661b1
Jimmy Huang <jimmy@netivism.com.tw>
2015-01-17 21:40:50 +0800

    Add pansharpening function.

--- pansharpening.sh
+++ pansharpening.sh
@@ -9,6 +9,7 @@ fi
 export ITK_AUTOLOAD_PATH=/usr/local/bin/
 export LD_LIBRARY_PATH=/usr/local/lib/otb/
 BASE=$1
+TMP=/tmp
 DIR=/media/sf_landsat8/$BASE
 cd /usr/local/bin
 gdalbuildvrt -separate -q -srcnodata "0 0 0" -vrtnodata "0 0 0" ${DIR}/rgb.vrt ${DIR}/${BASE}_B4.TIF ${DIR}/${BASE}_B3.TIF ${DIR}/${BASE}_B2.TIF
@@ -21,4 +22,9 @@ sed -i '18  i\
 sed -i '30 i\
 <ColorInterp>Blue</ColorInterp>' $DIR/rgb.vrt 
 
-otbcli_BundleToPerfectSensor -ram 4096 -inp ${DIR}/${BASE}_B8.TIF -inxs ${DIR}/rgb.vrt -out /tmp/pansharp.tif uint16
+otbcli_BundleToPerfectSensor -ram 4096 -inp ${DIR}/${BASE}_B8.TIF -inxs ${DIR}/rgb.vrt -out $TMP/pansharp.tif uint16
+gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $TMP/pansharp.tif $TMP/pansharp-scaled.tif
+gdalwarp -r cubic -wm 4096 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW -co tfw=yes $TMP/pansharp-scaled.tif $TMP/pansharp_3857.tif
+convert -sigmoidal-contrast 33x12% $TMP/pansharp_3857.tif $TMP/pansharp_3857-bright.tif
+cp $TMP/pansharp_3857.tfw $TMP/pansharp_3857-bright.tfw
+gdal_translate $TMP/pansharp_3857-bright.tif $TMP/harmony-rgb-pan.tif

dad10183e54e0375ae675c4739ac328c0b72a739
Jimmy Huang <jimmy@netivism.com.tw>
2015-01-27 06:36:55 +0800

    Use ethercalc for simple web queue.

--- download.sh
+++ download.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
+PENDING=1f8cfkxar1
+DOWNLOADED=wn9wlxessv
+
+# get queue list
+curl -s https://www.ethercalc.org/${PENDING}.csv | tr -d ',' > queue-pending.txt
+curl -s https://www.ethercalc.org/${DOWNLOADED}.csv | tr -d ',' > queue-downloaded.txt
 NAME=`head -n1 queue-pending.txt`
-URL=http://earthexplorer.usgs.gov/download/4923/$NAME/STANDARD/EE
+
+# download landsat
 mkdir -p `pwd`/data
-#wget --save-cookies /tmp/cookies.txt --post-data 'username=twlandsat&password=NwdBdCMF333p' \
-#  $URL \
-#  -O data/
+cd data
+echo "Downloading ${NAME}"
+# landsat download ${NAME}
+cd ../
+
+# update queue list
+#if [ -f ${NAME}.tar.gz ]; then
+  echo "$(tail -n +2 queue-pending.txt)" > queue-pending.txt
+  echo "${NAME}" >> queue-downloaded.txt
+  sed 's/$/,,/g' queue-pending.txt > queue-pending.csv
+  sed 's/$/,,/g' queue-downloaded.txt > queue-downloaded.csv
+  curl -X PUT -H 'Content-Type: text/csv' --data-binary @queue-pending.csv https://www.ethercalc.org/_/${PENDING}
+  curl -X PUT -H 'Content-Type: text/csv' --data-binary @queue-downloaded.csv https://www.ethercalc.org/_/${DOWNLOADED}
+#fi

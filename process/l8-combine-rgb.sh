2777be36ac858e9db8d06bffad905d718b1d81bc
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-18 07:59:40 +0800

    Adjust gamma value

--- process/l8-combine-rgb.sh
+++ process/l8-combine-rgb.sh
@@ -17,7 +17,7 @@ mkdir -p $TMP
 cp $1 $TMP/rgb-pan.tif
 
 # processing image
-convert -monitor -channel B -gamma 0.98 -channel R -gamma 1.02 -channel RGB -sigmoidal-contrast 50x12% $TMP/rgb-pan.tif $TMP/rgb-pan-light.tif
+convert -monitor -channel B -gamma 0.98 -channel R -gamma 1.02 -channel RGB -sigmoidal-contrast 50x12% -gamma 1.1 $TMP/rgb-pan.tif $TMP/rgb-pan-light.tif
 cp -f $TMP/rgb-pan-light.tif $FINAL
 
 # append geotiff into image

7fbfc09551416fb9645ec715c65230c51d657eba
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-28 16:17:53 +0800

    Auto detect image brightness for rgb.

--- process/l8-combine-rgb.sh
+++ process/l8-combine-rgb.sh
@@ -7,6 +7,7 @@ then
   exit 1
 fi
 
+THREADHOLD=40
 FINAL=$2
 FILENAME=`basename $2`
 NAME=${FILENAME%.*}
@@ -17,7 +18,17 @@ mkdir -p $TMP
 cp $1 $TMP/rgb-pan.tif
 
 # processing image
-convert -monitor -channel B -gamma 0.98 -channel R -gamma 1.02 -channel RGB -sigmoidal-contrast 30x13% -gamma 1.05 $TMP/rgb-pan.tif $TMP/rgb-pan-light.tif
+echo "Auto detect brightness, needs a few minutes to process large img ..."
+BRIGHT=`identify -verbose $TMP/final-rgb-pan.tif 2>/dev/null | grep mean | awk '{print $2}' | sed -n 5p`
+BRIGHT=${BRIGHT%.*}
+echo "Brightness is $BRIGHT, going to convert image..."
+
+if [ "$BRIGHT" -lt "$THREADHOLD" ]; then
+  convert -monitor -channel B -gamma 0.98 -channel R -gamma 1.02 -channel RGB -sigmoidal-contrast 50x13% -gamma 1.07 $TMP/rgb-pan.tif $TMP/rgb-pan-light.tif
+else
+  convert -monitor -channel B -gamma 0.98 -channel R -gamma 1.02 -channel RGB -sigmoidal-contrast 30x13% -gamma 1.05 $TMP/rgb-pan.tif $TMP/rgb-pan-light.tif
+fi
+
 cp -f $TMP/rgb-pan-light.tif $FINAL
 
 # append geotiff into image

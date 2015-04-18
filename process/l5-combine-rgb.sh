8b9ee5de243be86908dc8e8119ec6d42c32bd5a2
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-18 06:49:39 +0000

    fixes landsat 5

--- process/l5-combine-rgb.sh
+++ process/l5-combine-rgb.sh
@@ -24,13 +24,13 @@ done
 
 convert -combine $DIR/{3,2,1}-projected.tif $DIR/rgb.tif
 
-echo "Pan 4. Remove black border of image and scale"
-gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $DIR/rgb.tif $DIR/rgb-scaled.tif
-gdalwarp -r cubic -wm 1024 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW $DIR/rgb-scaled.tif $DIR/rgb-s.tif
-
 gdalwarp -t_srs EPSG:3857  $DIR/${LANDSAT}_B1.TIF $DIR/warp_B1.TIF
 listgeo -tfw $DIR/warp_B1.TIF
 mv $DIR/warp_B1.tfw $DIR/${NAME}.tfw
+
+cp ${NAME}.tfw rgb-scaled.tfw
+gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $DIR/rgb.tif $DIR/rgb-scaled.tif
+gdalwarp -r cubic -wm 1024 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW $DIR/rgb-scaled.tif $DIR/rgb-s.tif
 
 convert -monitor -channel B -gamma 0.6 -channel R -gamma 1.01 -channel RGB -sigmoidal-contrast 33x12% ${DIR}/rgb-s.tif ${DIR}/${NAME}.tif
 

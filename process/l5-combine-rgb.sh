c0040fed165b9e4b72da29c399b4439a675a3d1a
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-18 06:05:46 +0000

    Add landsat 5 support.

--- process/l5-combine-rgb.sh
+++ process/l5-combine-rgb.sh
@@ -13,32 +13,27 @@ FILENAME=`basename $2`
 NAME=${FILENAME%.*}
 LANDSAT=`basename $1`
 DIR=$1
-TMP="${DIR}/tmp"
-mkdir -p $TMP
 cd $DIR
 
-for BAND in {3,2,1}; do
+for BAND in {1,2,3}; do
   if [ ! -f $BAND-projected.tif ]; then
     FILE=$DIR/${LANDSAT}_B${BAND}.TIF
-    gdalwarp -t_srs EPSG:3857 $FILE $DIR/$BAND-projected.tif;
+    gdalwarp -t_srs EPSG:3857 $FILE $BAND-projected.tif;
   fi
 done
-convert -monitor -combine $DIR/{3,2,1}-projected.tif $TMP/rgb.tif
+
+convert -combine $DIR/{3,2,1}-projected.tif $DIR/rgb.tif
 
 echo "Pan 4. Remove black border of image and scale"
-gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $TMP/rgb.tif $TMP/rgb-scaled.tif
-gdalwarp -r cubic -wm 1024 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW $TMP/rgb-scaled.tif $TMP/rgb-s.tif
+gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $DIR/rgb.tif $DIR/rgb-scaled.tif
+gdalwarp -r cubic -wm 1024 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW $DIR/rgb-scaled.tif $DIR/rgb-s.tif
 
 gdalwarp -t_srs EPSG:3857  $DIR/${LANDSAT}_B1.TIF $DIR/warp_B1.TIF
 listgeo -tfw $DIR/warp_B1.TIF
 mv $DIR/warp_B1.tfw $DIR/${NAME}.tfw
 
-convert -monitor -channel B -gamma 0.6 -channel R -gamma 1.01 -channel RGB -sigmoidal-contrast 33x12% $TMP/rgb-s.tif $TMP/${NAME}.tif
+convert -monitor -channel B -gamma 0.6 -channel R -gamma 1.01 -channel RGB -sigmoidal-contrast 33x12% ${DIR}/rgb-s.tif ${DIR}/${NAME}.tif
 
-gdal_edit.py -a_srs EPSG:3857 $TMP/${NAME}.tif
-cp -f $TMP/${NAME}.tif $FINAL
+gdal_edit.py -a_srs EPSG:3857 ${DIR}/${NAME}.tif
+cp -f $DIR/${NAME}.tif $FINAL
 
-# clean up
-if [ -f $FINAL ]; then
-  rm -Rf $TMP
-fi

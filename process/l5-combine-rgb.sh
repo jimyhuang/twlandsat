08e76689252f1694942bdc4fb24c922739e5b038
jimmy huang <jimmy@netivism.com.tw>
2015-04-19 04:44:50 +0000

    Correct landsat 5 band color.

--- process/l5-combine-rgb.sh
+++ process/l5-combine-rgb.sh
@@ -29,7 +29,7 @@ listgeo -tfw $DIR/warp_B1.TIF
 mv $DIR/warp_B1.tfw $DIR/${NAME}.tfw
 
 cp ${NAME}.tfw rgb-scaled.tfw
-gdal_translate -ot Byte -scale 0 65535 0 255 -a_nodata "0 0 0" $DIR/rgb.tif $DIR/rgb-scaled.tif
+gdal_translate -a_nodata "0 0 0" $DIR/rgb.tif $DIR/rgb-scaled.tif
 gdalwarp -r cubic -wm 1024 -multi -srcnodata "0 0 0" -dstnodata "0 0 0" -dstalpha -wo OPTIMIZE_SIZE=TRUE -wo UNIFIED_SRC_NODATA=YES -t_srs EPSG:3857 -co TILED=YES -co COMPRESS=LZW $DIR/rgb-scaled.tif $DIR/rgb-s.tif
 
 convert -monitor -channel B -gamma 0.6 -channel R -gamma 1.01 -channel RGB -sigmoidal-contrast 33x12% ${DIR}/rgb-s.tif ${DIR}/${NAME}.tif

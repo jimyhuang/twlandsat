de406f37b30b711da88cd279d98b093a11fa8da5
Jimmy Huang <jimmy@netivism.com.tw>
2015-04-19 06:57:39 +0200

    Mapping git dir into docker.

--- docker-start.sh
+++ docker-start.sh
@@ -3,6 +3,8 @@
 STARTED=`docker ps | grep twlandsat`
 STOPPED=`docker ps -a -f exited=0 | grep twlandsat`
 
+PWD=`pwd`
+
 if [ -n "$STARTED" ]; then
   echo "Docker attach exists container ... "
   docker attach twlandsat
@@ -16,6 +18,11 @@ if [ -n "$STOPPED" ]; then
 fi
 if [ ! $STARTED ] && [ ! $STOPPED ]; then
   echo "Docker run ... "
-  docker run --name twlandsat -e "RSYNC_PASSWORD=1A5%QfFx%S8%" -v /home/landsat:/root/landsat -i -t jimyhuang/twlandsat /bin/bash
+  docker run \
+    --name twlandsat \
+    -e "RSYNC_PASSWORD=1A5%QfFx%S8%" \
+    -v /home/landsat:/root/landsat \
+    -v $PWD:/home/twlandsat \
+    -i -t jimyhuang/twlandsat /bin/bash
   exit
 fi

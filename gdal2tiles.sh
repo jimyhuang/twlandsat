163d1aaa238d7569827a678671d6bcbe7e6bcfb5
Jimmy Huang <jimmy@netivism.com.tw>
2016-10-28 07:47:21 +0800

    Add two entrypoint for test.

---
+++ gdal2tiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Usage:
+#   docker run -rm --entrypoint /home/twlandsat/gdal2tiles.sh jimyhuang/twlandsat LC81170442015336LGN00 final-rgb.TIF tiles-rgb
+
+NAME=$1
+FROM=$2
+TO=$3
+
+cd ~/landsat/processed/{$NAME}
+gdal2tiles.py $FROM $TO
---
+++ imagemagick.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Usage:
+#   LC81170442015336LGN00 final-rgb.TIF tiles-rgb
+#   docker run -rm --entrypoint -v ~/landsat/:/root/landsat /home/twlandsat/imagemagick-convert.sh jimyhuang/twlandsat LC81170442015336LGN00 final-rgb.TIF final-rgb.jpg
+
+NAME=$1
+FROM=$2
+TO=$3
+
+cd ~/landsat/processed/{$NAME}
+convert $1 $2

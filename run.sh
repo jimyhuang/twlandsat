be53b6e19e90bb0be07dee1693d23f1b825fc364
Jimmy Huang <jimmy@netivism.com.tw>
2016-10-30 14:03:05 +0800

    Update run script for better priority and color.

--- run.sh
+++ run.sh
@@ -12,7 +12,6 @@ function unshift {
   FIRST=`head -1 $FILE`
   tail -n +2 "$FILE" > $HOME/.landsat-queue.swp
   cp $HOME/.landsat-queue.swp $FILE
-  rm $HOME/.landsat-queue.swp
   echo $FIRST
 }
 
@@ -28,13 +27,14 @@ fi
 echo "" > $RUNNING
 
 if [ ! $1 ]; then
-  echo -e "\e[33m$dt\e[0m Not specify landsat id, use ~/.landsat-queue instead"
-  if [ !-f $QUEUE ]; then
+  echo -e "\e[33m$dt\e[0m Not specify landsat id, use ~/.landsat-queue instead."
+  if [ ! -f $QUEUE ]; then
     echo -e "\e[33m$dt\e[0m File not found at $HOME/.landsat-queue, stop."
     exit 0
   else
     LAST=`unshift $QUEUE`
-    if [ -n "$last" ] && [ ! -d "$OUTPUTDIR/procssed/$LANDSAT" ]; then
+    echo $LAST;
+    if [ -n "$LAST" ] && [ ! -f "$OUTPUTDIR/procssed/$LAST/rgb.jpg" ]; then
       LANDSAT=$LAST
     fi
   fi
@@ -43,37 +43,37 @@ else
 fi
 
 if [ -z "$LANDSAT" ]; then
-  echo "\e[31m$dt\e[0m Cannot find any landsat id in queue or argument"
+  echo -e "\e[31m$dt\e[0m Cannot find any landsat id in queue or argument"
   exit 1
 else
   echo "$LANDSAT" > $RUNNING
 fi
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[42m$dt Start process $LANDSAT e[0m"
-echo -e "\e[32m$dt\e[0m Download"
+echo -e "\e[42m$dt Start process $LANDSAT\e[0m"
+echo -e "\e[34m$dt\e[0m Download"
 docker run --rm -it -v $OUTPUTDIR:/root/landsat developmentseed/landsat-util:latest landsat download $LANDSAT -b 2345
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[32m$dt\e[0m Process rgb"
+echo -e "\e[34m$dt\e[0m Process rgb"
 docker run --rm -it -v $OUTPUTDIR:/root/landsat developmentseed/landsat-util:latest landsat process /root/landsat/downloads/$LANDSAT
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[32m$dt\e[0m Process NDVI"
+echo -e "\e[34m$dt\e[0m Process NDVI"
 docker run --rm -it -v $OUTPUTDIR:/root/landsat developmentseed/landsat-util:latest landsat process /root/landsat/downloads/$LANDSAT --ndvi
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[32m$dt\e[0m Generate tiles"
+echo -e "\e[34m$dt\e[0m Generate tiles"
 docker run --rm --entrypoint /home/twlandsat/scripts/gdal2tiles.sh -v $OUTPUTDIR:/root/landsat jimyhuang/twlandsat $LANDSAT ${LANDSAT}_bands_432.TIF tiles-rgb
 docker run --rm --entrypoint /home/twlandsat/scripts/gdal2tiles.sh -v $OUTPUTDIR:/root/landsat jimyhuang/twlandsat $LANDSAT ${LANDSAT}_NDVI.TIF tiles-ndvi
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[32m$dt\e[0m Generate preview"
-docker run --rm --entrypoint /home/twlandsat/scripts/imagemagick.sh -v $OUTPUTDIR:/root/landsat jimyhuang/twlandsat $LANDSAT ${LANDSAT}_bands_432.TIF -scale 25% rgb.jpg
+echo -e "\e[34m$dt\e[0m Generate preview"
 docker run --rm --entrypoint /home/twlandsat/scripts/imagemagick.sh -v $OUTPUTDIR:/root/landsat jimyhuang/twlandsat $LANDSAT ${LANDSAT}_NDVI.TIF -scale 25% ndvi.jpg
+docker run --rm --entrypoint /home/twlandsat/scripts/imagemagick.sh -v $OUTPUTDIR:/root/landsat jimyhuang/twlandsat $LANDSAT ${LANDSAT}_bands_432.TIF -scale 25% rgb.jpg
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
-echo -e "\e[32m$dt\e[0m Final clear"
+echo -e "\e[34m$dt\e[0m Final clear"
 echo "" > $RUNNING
 if [ -d "$OUTPUTDIR/downloads/$LANDSAT" ]; then
   rm -Rf "$OUTPUTDIR/downloads/$LANDSAT"

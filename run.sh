fcb3b39ad612ec65281dde649a89dcb286dc9080
Jimmy Huang <jimmy@netivism.com.tw>
2016-11-06 23:36:23 +0800

    fixes typo of imagemagick output.

--- run.sh
+++ run.sh
@@ -85,7 +85,7 @@ echo -e "\e[34m$dt\e[0m Generate preview"
 cd $OUTPUTDIR/processed/$LANDSAT && convert ${LANDSAT}_NDVI.TIF -scale 600 -fuzz 5% -transparent black ndvi.png
 cd $OUTPUTDIR/processed/$LANDSAT && convert ${LANDSAT}_NDVI.TIF -scale 1920 ndvi.jpg
 cd $OUTPUTDIR/processed/$LANDSAT && convert ${LANDSAT}_bands_432.TIF -scale 600 -fuzz 5% -transparent black rgb.png
-cd $OUTPUTDIR/processed/$LANDSAT && convert ${LANDSAT}_bands_432.TIF -scale 1920 rgb.png
+cd $OUTPUTDIR/processed/$LANDSAT && convert ${LANDSAT}_bands_432.TIF -scale 1920 rgb.jpg
 
 dt=`date '+%Y-%m-%d %H:%M:%S'`
 echo -e "\e[34m$dt\e[0m Final clear"
